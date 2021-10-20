574580763db53d11fadbab7375bf6a7c634c599d
TriceHelix <teamnebulargd@gmail.com>
2021-10-20 16:01:54 +0200

    Fix Windows compatibility

--- src/cuda_block_solver.cu
+++ src/cuda_block_solver.cu
@@ -30,6 +30,40 @@ namespace cuba
 namespace gpu
 {
 
+// atomicAdd IMPLEMENTATION FOR OLD COMPUTE CAPABILITY
+#if __CUDA_ARCH__ < 600
+	__device__ double atomicAdd(double* address, double val)
+	{
+		unsigned long long int* address_as_ull =
+			(unsigned long long int*)address;
+		unsigned long long int old = *address_as_ull, assumed;
+
+		do {
+			assumed = old;
+			old = atomicCAS(address_as_ull, assumed,
+				__double_as_longlong(val +
+					__longlong_as_double(assumed)));
+
+		} while (assumed != old);
+
+		return __longlong_as_double(old);
+	}
+
+	__device__ int atomicAdd(int* address, int val)
+	{
+		int guess = *address;
+		int oldValue = atomicCAS(address, guess, guess + val);
+
+		while (oldValue != guess)
+		{
+			guess = oldValue;
+			oldValue = atomicCAS(address, guess, guess + val);
+		}
+
+		return oldValue;
+	}
+#endif
+
 ////////////////////////////////////////////////////////////////////////////////////
 // Type alias
 ////////////////////////////////////////////////////////////////////////////////////
